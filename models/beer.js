73663f894f4d6c4defc122069a18e59bb38f2445
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-02-06 10:26:31 -0600

    Set up Mondo

---
+++ models/beer.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BeerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: [0, 'Rating must be a number between 0 and 10'],
+        max:[10, 'Rating must be a number between 0 and 10'],
+        required: true
+    }
+});
+
+module.exports = mongoose.Model('Beer', BeerSchema);
